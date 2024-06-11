0760017f2e69143c037163714119758f9c0ae7ba
Md. Shakil <shakil.eub.cse@gmail.com>
2024-06-11 20:47:39 +0600

    Create To Do project model

--- todoproject/todoapp/admin.py
+++ todoproject/todoapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from todoapp.models import *
 
 # Register your models here.
+class CustomUserDisplay(admin.ModelAdmin):
+    list_display = ['username','first_name','email']
+    search_fields = ['username','email']
+    
+    fieldsets = [
+        (
+            "User Information",
+        {
+            "fields":["username","first_name","last_name","email","password"]
+        }
+        )
+    ]
+
+admin.site.register(CustomToDoUserModel,CustomUserDisplay)
+
+class categoryDisplay(admin.ModelAdmin):
+    list_display = ['CategoryName','user']
+admin.site.register(CategoryModel,categoryDisplay)
+admin.site.register(TaskModel)
